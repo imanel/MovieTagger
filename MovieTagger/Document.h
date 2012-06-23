de3ff25ce99957fef332abb2ff4d15c24224c6d1
Bernard Potocki <bernard.potocki@imanel.org>
2012-06-23 12:50:35 +0200

    Initial Commit

---
+++ MovieTagger/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  MovieTagger
+//
+//  Created by Bernard Potocki on 23.06.2012.
+//  Copyright (c) 2012 Rebased. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ MovieTagger/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MovieTagger
+//
+//  Created by Bernard Potocki on 23.06.2012.
+//  Copyright (c) 2012 Rebased. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
