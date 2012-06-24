54fb3f5930f0b1335a125520cb4beea2b1cb9bf0
Bernard Potocki <bernard.potocki@imanel.org>
2012-06-24 16:21:17 +0200

    Load real data with search action

--- MovieTagger/MoviePreview.m
+++ MovieTagger/MoviePreview.m
@@ -13,20 +13,46 @@
 @synthesize externalID, title, year, source;
 
 + (NSArray *)findByTitle:(NSString *)t source:(NSString *)s {
-    NSMutableArray *result = [[NSMutableArray alloc] init];
-    [result addObject:[[self alloc] initWithExternalID:@"12"
-                                                 title:@"test 1"
-                                                  year:@"1999"
-                                                source:s]];
-    [result addObject:[[self alloc] initWithExternalID:@"13"
-                                                 title:@"test 2"
-                                                  year:@"2000"
-                                                source:s]];
-    [result addObject:[[self alloc] initWithExternalID:@"14"
-                                                 title:@"test 3"
-                                                  year:@"2001"
-                                                source:s]];
-    return result;
+    NSArray *queryResults = [self queryForTitle:t source:s];
+    
+    if(queryResults == NULL) {
+        NSRunInformationalAlertPanel(@"MovieTagger", @"Could not load data. Please try again later.", @"OK", nil, nil);
+        return [[NSArray alloc] init];
+    } else {
+        NSMutableArray *results = [[NSMutableArray alloc] init];
+        for(id queryResult in queryResults) {
+            [results addObject:[[self alloc] initWithExternalID:[queryResult objectForKey:@"id"]
+                                                          title:[queryResult objectForKey:@"title"]
+                                                           year:[queryResult objectForKey:@"year"]
+                                                         source:s]];
+        }
+        return results;
+    }
+}
+
++ (NSArray *)queryForTitle:(NSString *)t source:(NSString *)s {
+    NSURL *url = [self urlForTitle:t source:s];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    NSData *response = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSString *json_string = [[NSString alloc] initWithData:response encoding:NSUTF8StringEncoding];
+    
+    NSError *error = NULL;
+    NSDictionary *result = [NSJSONSerialization JSONObjectWithData:[json_string dataUsingEncoding:NSUTF8StringEncoding] options:0 error:&error];
+    
+    if(error != NULL) {
+        return NULL;
+    } else if(![[result objectForKey:@"status"] isEqual:@"success"]) {
+        return NULL;
+    } else {
+        return [result objectForKey:@"result"];
+    }
+}
+
++ (NSURL *)urlForTitle:(NSString *)t source:(NSString *)s {
+    CFStringRef escapeChars = (CFStringRef) @":/?#[]@!$&’()*+,;=";
+    NSString *titleForSearch = (__bridge_transfer NSString *) CFURLCreateStringByAddingPercentEscapes(NULL, (__bridge_retained CFStringRef) t, NULL, escapeChars, kCFStringEncodingUTF8);
+    NSString *urlString = [[NSString alloc] initWithFormat:@"http://unified-db.heroku.com/api?b=%@&t=%@", s, titleForSearch];
+    return [NSURL URLWithString:urlString];
 }
 
 - (id)initWithExternalID:(NSString *)eID title:(NSString *)t year:(NSString *)y source:(NSString *)s {
